f64b750cb9297431d9100a29e225b2e27da06d63
gnom4ik <jr.pashkin@gmail.com>
2017-03-26 00:09:13 +0300

    v2

--- bot.py
+++ bot.py
@@ -4,20 +4,24 @@ import dict
 import telegram
 import config
 import func
+import time
 
 bot = telegram.Bot(token=config.TOKEN)
-updates = bot.getUpdates()
+updates = bot.getUpdates(offset=config.offset)
 func.get_last_id(updates)
 count = config.messages_id[-1]
 
 while config.last:
-    updates = bot.getUpdates()
+    updates = bot.getUpdates(offset=config.offset)
     func.get_last_id(updates)
     if count != config.messages_id[-1]:
         func.get_last(updates)
         config.last = False
         dict.dictionary(config.messages[-1])
-
+        config.offset = count
+    config.offset += 1
+    time.sleep(5)
+    print(config.offset, count)
 
 
 
--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 
 chat_id = '-153240988'
 TOKEN = '312447659:AAHYz0X93nDRDbZQSb2oxgaoGZH8cuzlfdU'
+last = True
+offset = 0
 messages = []
 messages_id = []
-last = True

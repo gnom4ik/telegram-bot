c2112367a1bcd90ab38a692d58992c7c029ecd4c
gnom4ik <jr.pashkin@gmail.com>
2017-03-26 22:22:29 +0300

    v1

--- bot.py
+++ bot.py
@@ -16,12 +16,10 @@ while config.last:
     func.get_last_id(updates)
     if count != config.messages_id[-1]:
         func.get_last(updates)
-        #config.last = False
         dict.dictionary(config.messages[-1])
         config.offset = count
     if config.offset == count:
         time.sleep(5)
-        print(config.offset, count)
     else:
         config.offset += 1
         time.sleep(5)
